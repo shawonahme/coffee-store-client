ea3732d8b535cf427f618b2e3149cc86fa2f25ee
shawonahme <shawonahmed55660@gmail.com>
2024-12-02 15:38:51 +0600

    heelo

--- src/allComponents/Header.jsx
+++ src/allComponents/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
     <div>
       <div className="navbar bg-base-100">
         <div className="flex-1">
-          <a className="btn btn-ghost text-xl">daisyUI</a>
+          <a className="btn btn-ghost text-xl">Coffee House</a>
         </div>
         <div className="flex-none">
           <ul className="menu menu-horizontal px-1">
